7aca17d2e47e5c5857727224ac2e0651b13d7f83
dvillarpsl <dvillar@psl.com.co>
2018-01-28 17:15:07 -0500

    Fixed the carousel design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,44 +19,16 @@ export class HomeComponent implements OnInit {
 
     this.carouselBanner = {
       grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
-      slide: 4,
-      speed: 500,
-      interval: 5000,
+      slide: 1,
+      speed: 400,
+      interval: 10000,
       point: {
-        visible: true,
-        pointStyles: `
-          .ngxcarouselPoint {
-            list-style-type: none;
-            text-align: center;
-            padding: 12px;
-            margin: 0;
-            white-space: nowrap;
-            overflow: auto;
-            position: absolute;
-            width: 100%;
-            bottom: 20px;
-            left: 0;
-            box-sizing: border-box;
-          }
-          .ngxcarouselPoint li {
-            display: inline-block;
-            border-radius: 999px;
-            background: rgba(255, 255, 255, 0.55);
-            padding: 5px;
-            margin: 0 3px;
-            transition: .4s ease all;
-          }
-          .ngxcarouselPoint li.active {
-              background: white;
-              width: 10px;
-          }
-        `
+        visible: true
       },
       load: 2,
       custom: 'banner',
       touch: true,
-      loop: false,
-      easing: 'cubic-bezier(0, 0, 0.2, 1)'
+      loop: true
     };
   }
 
